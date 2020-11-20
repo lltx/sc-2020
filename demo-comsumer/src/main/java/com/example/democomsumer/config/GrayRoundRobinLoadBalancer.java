79eed334ebc691555a95545d3f649756c0763558
lengleng <wangiegie@gmail.com>
2020-11-20 20:24:56 +0800

    2020年11月20日11:01:51

--- demo-comsumer/src/main/java/com/example/democomsumer/config/GrayRoundRobinLoadBalancer.java
+++ demo-comsumer/src/main/java/com/example/democomsumer/config/GrayRoundRobinLoadBalancer.java
@@ -3,15 +3,16 @@ package com.example.democomsumer.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.DefaultRequestContext;
-import org.springframework.cloud.client.loadbalancer.Request;
-import org.springframework.cloud.client.loadbalancer.Response;
+import org.springframework.cloud.client.loadbalancer.*;
+import org.springframework.cloud.loadbalancer.core.NoopServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author lengleng
@@ -19,6 +20,11 @@ import java.util.Map;
  */
 @Slf4j
 public class GrayRoundRobinLoadBalancer extends RoundRobinLoadBalancer {
+
+    private ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider;
+
+    private String serviceId;
+
     /**
      * @param serviceInstanceListSupplierProvider a provider of
      *                                            {@link ServiceInstanceListSupplier} that will be used to get available instances
@@ -26,26 +32,37 @@ public class GrayRoundRobinLoadBalancer extends RoundRobinLoadBalancer {
      */
     public GrayRoundRobinLoadBalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId) {
         super(serviceInstanceListSupplierProvider, serviceId);
+        this.serviceInstanceListSupplierProvider = serviceInstanceListSupplierProvider;
+        this.serviceId = serviceId;
     }
 
-    /**
-     * @param serviceInstanceListSupplierProvider a provider of
-     *                                            {@link ServiceInstanceListSupplier} that will be used to get available instances
-     * @param serviceId                           id of the service for which to choose an instance
-     * @param seedPosition                        Round Robin element position marker
-     */
-    public GrayRoundRobinLoadBalancer(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId, int seedPosition) {
-        super(serviceInstanceListSupplierProvider, serviceId, seedPosition);
-    }
 
     @Override
     public Mono<Response<ServiceInstance>> choose(Request request) {
+        ServiceInstanceListSupplier supplier = serviceInstanceListSupplierProvider
+                .getIfAvailable(NoopServiceInstanceListSupplier::new);
+        return supplier.get(request).next().map(serviceInstances -> getInstanceResponse(serviceInstances, request));
+
+    }
+
+    Response<ServiceInstance> getInstanceResponse(List<ServiceInstance> instances, Request request) {
         DefaultRequestContext requestContext = (DefaultRequestContext) request.getContext();
 
         feign.Request feignReq = (feign.Request) requestContext.getClientRequest();
 
         Map<String, Collection<String>> headers = feignReq.headers();
-        return super.choose(request);
+        // 灰度匹配逻辑
+        if (instances.isEmpty()) {
+            if (log.isWarnEnabled()) {
+                log.warn("No servers available for service: " + serviceId);
+            }
+            return new EmptyResponse();
+        }
+        // TODO: enforce order?
+        int pos = new Random().nextInt(1000);
+        ServiceInstance instance = instances.get(pos % instances.size());
+
+        return new DefaultResponse(instance);
     }
 
 }
